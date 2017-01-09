d532b9b2dbae3f89f612335bd506ec2778ca5db7
Matt Callahan <mattcllhn@gmail.com>
2017-01-09 09:09:40 -0600

    added data to data.json

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var fs = require('fs');
 
 app.use(express.static('public'));
 
@@ -13,4 +13,15 @@ app.listen(3000,function(){
 });
 app.get('/',function(req, res){
   res.sendFile(path.resolve('public/views/index.html'));
+});
+app.get('/json',function(req, res){
+fs.readFile('data.json', function(err,data){
+  if(err){
+    console.log(err);
+  }else{
+    console.log(data);
+
+  }
+  res.sendStatus(200);
+});
 });
