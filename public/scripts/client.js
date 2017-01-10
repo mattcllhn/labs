3f793849e97fbe9c8d2ce092d444ae5d104ca880
Matt Callahan <mattcllhn@gmail.com>
2017-01-10 12:22:50 -0600

    stash

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,45 @@
 console.log('js');
 var myApp = angular.module('myApp',[]);
-myApp.controller('mockController', ['$scope',function($scope){
+myApp.controller('mockController', ['$scope','$http',function($scope,$http){
   console.log('NG');
 $scope.words = {
   hello:'hello',
   world:'earth'
 };
+$scope.classes = {
+  collapse:true
+};
   $scope.toggleDropdown = function(el){
-    // console.log('click works',el);
-    // el.toggleClass('green');
+    console.log('click works',el);
+    el.classes.collapse = !el.classes.collapse;
 
   };//toggleDropdown
 
+
+  $scope.getData = function(){
+    $http({
+      method:'GET',
+      url:'/jsonData'
+    }).then(function(data){
+      console.log(data);
+    });
+  };
+
+  
+  $scope.fsTest = function(){
+    var objToSend = {
+    id:1,
+    otherThing:'is a string',
+    lastThing:true
+    };
+    $http({
+      method:'POST',
+      url:'/test',
+      data:objToSend
+    }).then(function(data){
+      console.log(data);
+    });
+  };
 
 
 }])//mockController
--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser');
+var jsonData = require(path.resolve('data.json'));
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use('/bootstrap', express.static(path.resolve ('./node_modules/bootstrap/dist')));
@@ -14,14 +18,19 @@ app.listen(3000,function(){
 app.get('/',function(req, res){
   res.sendFile(path.resolve('public/views/index.html'));
 });
-app.get('/json',function(req, res){
-fs.readFile('data.json', function(err,data){
-  if(err){
-    console.log(err);
-  }else{
-    console.log(data);
 
-  }
-  res.sendStatus(200);
+app.get('/jsonData',function(req, res){
+  console.log(jsonData);
+    res.send(jsonData);
+    // res.sendStatus(200);
+  });
+
+app.post('/test',function(req,res){
+  var data = req.body;
+  console.log(data);
+  fs.writeFile('testeroo.json', JSON.stringify(data), function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > testeroo.json');
 });
+  res.sendStatus(201);
 });
